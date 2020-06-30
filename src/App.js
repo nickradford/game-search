d3cd01c242d5e616e96e1c5f9edd135fdddef40c
Nick Radford <nick@nicholasradford.com>
2020-06-30 11:47:33 -0700

    initial pass, working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const formatSearchQuery = (title = "", query = "") => {
+  const space = /\s/gi;
+  return `${title} ${query}`.trim().replace(space, "+");
+};
+
+const getGoogleUrl = (sq = "") => {
+  return `https://google.com/search?q=${sq}`;
+};
+
+const getDuckDuckGoUrl = (sq = "") => {
+  return `https://duckduckgo.com/?q=${sq}`;
+};
 
 function App() {
+  let [title, setTitle] = useState();
+  let [query, setQuery] = useState();
+  let [debug, setDebug] = useState();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Game Search</h1>
       </header>
+      <main>
+        <div>
+          <label htmlFor="game-title">Game Title</label>&nbsp;
+          <input
+            id="game-title"
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="query">Search Query</label>&nbsp;
+          <input
+            type="text"
+            id="query"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+        </div>
+        <div>
+          {(() => {
+            const q = formatSearchQuery(title, query);
+
+            return (
+              title &&
+              query && (
+                <>
+                  <a href={getGoogleUrl(q)} className="btn">
+                    Google
+                  </a>
+                  <a href={getDuckDuckGoUrl(q)} className="btn">
+                    DuckDuckGo
+                  </a>
+                </>
+              )
+            );
+          })()}
+        </div>
+      </main>
+      <aside>
+        <pre onClick={() => setDebug(!debug)}>debug</pre>
+        {debug && (
+          <div>
+            <p>Game: {title}</p>
+            <p>Query: {query}</p>
+
+            <p>Search Query: {formatSearchQuery(title, query)}</p>
+            <p>Google: {getGoogleUrl(formatSearchQuery(title, query))}</p>
+            <p>
+              DuckDuckGo: {getDuckDuckGoUrl(formatSearchQuery(title, query))}
+            </p>
+          </div>
+        )}
+      </aside>
     </div>
   );
 }
