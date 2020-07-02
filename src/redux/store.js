78fe7040b97bf9bb4394a84c9ba0fb36ad9d7700
Nick Radford <nick@nicholasradford.com>
2020-07-02 16:28:24 -0700

    devtools package

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,18 +1,19 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 
 import { rootReducer } from "./reducers";
 
-export function configureStore(preloadedState) {
+export function configureStore(preloadedState = {}) {
   const middlewares = [thunk];
   const middlewareEnhancer = applyMiddleware(...middlewares);
 
   const enhancers = [
     middlewareEnhancer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION__(),
+    // window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    //   window.__REDUX_DEVTOOLS_EXTENSION__(),
   ];
-  const composedEnhancers = compose(...enhancers);
+  const composedEnhancers = composeWithDevTools(...enhancers);
 
   return createStore(rootReducer, preloadedState, composedEnhancers);
 }
