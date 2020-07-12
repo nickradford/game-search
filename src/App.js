8d50462edf6c5a84235aafc9ec205914a6bbe604
Nick Radford <nick@nicholasradford.com>
2020-07-11 20:44:41 -0700

    remove unused code in original app

--- src/App.js
+++ src/App.js
@@ -1,32 +1,11 @@
-import React, { useState, useCallback } from "react";
+import React, { useState } from "react";
 import { Switch, Route, Link } from "react-router-dom";
 import { connect } from "react-redux";
 
 import SearchPage from "./pages/search";
 import GamePage from "./pages/game";
 
-import { searchForGame, findGameByTitle } from "./util/rawg";
-import { rdr } from "./rdr";
 import ImageTransition from "./components/image-transition";
-
-const formatSearchQuery = (title = "", query = "") => {
-  const space = /\s/gi;
-  return `${title} ${query}`.trim().replace(space, "+");
-};
-
-const getGoogleUrl = (sq = "") => {
-  return `https://google.com/search?q=${sq}`;
-};
-
-const getDuckDuckGoUrl = (sq = "") => {
-  return `https://duckduckgo.com/?q=${sq}`;
-};
-
-const APPLICATION_STATE = {
-  START: 1,
-  LISTING: 2,
-  SEARCHING: 3,
-};
 
 const mapStateToProps = ({ games }) => {
   let selectedGameImage;
@@ -43,14 +22,6 @@ const mapStateToProps = ({ games }) => {
 };
 
 function App({ selectedGameImage }) {
-  let [title, setTitle] = useState();
-  let [query, setQuery] = useState();
-  let [debug, setDebug] = useState();
-  let [selectedGame, setSelectedGame] = useState();
-  let [applicationState, setApplicationState] = useState(
-    APPLICATION_STATE.START
-  );
-
   return (
     <div className="w-full h-full bg-gray-900 text-white">
       <div
@@ -86,24 +57,7 @@ function App({ selectedGameImage }) {
           </Route>
         </Switch>
 
-        <footer className="flex justify-between">
-          <aside>
-            <pre onClick={() => setDebug(!debug)}>debug</pre>
-            {debug && (
-              <div>
-                <p>Game: {title}</p>
-                <p>Query: {query}</p>
-
-                <p>Search Query: {formatSearchQuery(title, query)}</p>
-                <p>Google: {getGoogleUrl(formatSearchQuery(title, query))}</p>
-                <p>
-                  DuckDuckGo:{" "}
-                  {getDuckDuckGoUrl(formatSearchQuery(title, query))}
-                </p>
-                <pre>{JSON.stringify(selectedGame, null, 4)}</pre>
-              </div>
-            )}
-          </aside>
+        <footer className="flex justify-end">
           <main>
             Game data provided by{" "}
             <a href="https://rawg.io" target="_new">
