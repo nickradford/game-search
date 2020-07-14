3fe4df85e42835a0e29abf2101cc822c9b0ab3b7
Nick Radford <nick@nicholasradford.com>
2020-07-13 21:04:38 -0700

    use redux-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,27 @@
 import { configureStore } from "@reduxjs/toolkit";
 import thunk from "redux-thunk";
-
 import { combineReducers } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import { gamesSlice } from "./slices/games";
 import { favoritesSlice } from "./slices/favorites";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
 
 export const rootReducer = combineReducers({
   games: gamesSlice.reducer,
   favorites: favoritesSlice.reducer,
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
   middleware: [thunk],
 });
+
+export const persistor = persistStore(store);
