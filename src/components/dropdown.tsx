7eb2192108b3d6cc708c6632c083af1c39006d6f
Nick Radford <nick@nicholasradford.com>
2020-07-17 11:06:41 -0700

    show favorites menu dropdown

---
+++ src/components/dropdown.tsx
@@ -0,0 +1,83 @@
+import * as React from "react";
+import cn from "classnames";
+import useOnClickOutside from "use-onclickoutside";
+
+interface BaseProps {
+  className?: string;
+  children: React.ReactNode;
+}
+
+const DropdownContext = React.createContext({
+  isOpen: false,
+  toggleIsOpen: () => {},
+});
+
+interface DropdownProps extends BaseProps {}
+export const Dropdown = ({ className, children }: DropdownProps) => {
+  const [isOpen, setIsOpen] = React.useState(false);
+  const ref = React.useRef(null);
+  useOnClickOutside(ref, () => setIsOpen(false));
+
+  return (
+    <DropdownContext.Provider
+      value={{
+        isOpen,
+        toggleIsOpen: () => {
+          setIsOpen(!isOpen);
+        },
+      }}
+    >
+      <div ref={ref} className={`md:relative z-50 ${className}`}>
+        {children}
+      </div>
+    </DropdownContext.Provider>
+  );
+};
+
+interface DropdownLabelProps extends BaseProps {}
+
+function Label({ className = "", children }: DropdownLabelProps) {
+  const ctx = React.useContext(DropdownContext);
+
+  const labelClasses = cn("cursor-pointer", className);
+
+  return (
+    <span className={labelClasses} onClick={() => ctx.toggleIsOpen()}>
+      {children}
+    </span>
+  );
+}
+
+Dropdown.Label = Label;
+
+interface DropdownMenuProps extends BaseProps {}
+function Menu({ className = "", children }: DropdownMenuProps) {
+  const ctx = React.useContext(DropdownContext);
+
+  const menuClasses = cn(
+    "absolute mx-4 md:mx-0 md:w-64 left-0 right-0 md:left-auto bg-black bg-opacity-75 rounded py-4",
+    {
+      hidden: ctx.isOpen === false,
+      block: ctx.isOpen,
+    },
+    className
+  );
+  return <div className={menuClasses}>{children}</div>;
+}
+
+Dropdown.Menu = Menu;
+
+interface DropdownMenuItemProps extends BaseProps {}
+function MenuItem({ children }: DropdownMenuItemProps) {
+  const ctx = React.useContext(DropdownContext);
+  const menuItemClasses = cn(
+    "hover:bg-white hover:text-black cursor-pointer px-4 py-2"
+  );
+  return (
+    <div className={menuItemClasses} onClick={() => ctx.toggleIsOpen()}>
+      {children}
+    </div>
+  );
+}
+
+Dropdown.MenuItem = MenuItem;
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,32 +1,55 @@
-import * as React from 'react';
-import { Link } from 'react-router-dom';
+import * as React from "react";
+import { Link } from "react-router-dom";
 
-import {GamePad} from './gamepad';
-import { RAWGGame } from '../interfaces/game';
+import { GamePad } from "./gamepad";
+import { RAWGGame } from "../interfaces/game";
+
+import { Dropdown } from "./dropdown";
 
 interface HeaderProps {
-  onClick?: ((event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => void);
+  onClick?: (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => void;
   favorites?: RAWGGame[];
 }
 
-export const Header = ({onClick, favorites = []}: HeaderProps) => {
-  return <header className="text-white font-asap ">
-  <div className="container mx-auto flex flex-wrap py-5 flex-col md:flex-row items-center">
-    <Link className="flex title-font font-medium items-center text-white mb-4 md:mb-0" to='/' onClick={onClick}>
-      <GamePad />
-      <span className="ml-3 text-xl font-bold italic">Game Search</span>
-    </Link>
-    <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center text-gray-300">
-      {/* <a className="mr-5 hover:text-white">First Link</a> */}
-      { favorites!.length ? <a className="mr-5 hover:text-white">Favorites</a> : null }
-      <Link className="mr-5 hover:text-white" to='/privacy'>Privacy</Link> 
-      <Link className="hover:text-white" to='/settings'>Settings</Link>
-    </nav>
-    {/* <button className="inline-flex items-center bg-gray-200 border-0 py-1 px-3 focus:outline-none hover:bg-gray-300 rounded text-base mt-4 md:mt-0">Button
+export const Header = ({ onClick, favorites = [] }: HeaderProps) => {
+  return (
+    <header className="text-white font-asap ">
+      <div className="container mx-auto flex flex-wrap py-5 flex-col md:flex-row items-center">
+        <Link
+          className="flex title-font font-medium items-center text-white mb-4 md:mb-0"
+          to="/"
+          onClick={onClick}
+        >
+          <GamePad />
+          <span className="ml-3 text-xl font-bold italic">Game Search</span>
+        </Link>
+        <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center text-gray-300">
+          {/* <a className="mr-5 hover:text-white">First Link</a> */}
+          {favorites!.length ? (
+            <Dropdown className="mr-5">
+              <Dropdown.Label>Favorites</Dropdown.Label>
+              <Dropdown.Menu>
+                {favorites.map((game) => (
+                  <Dropdown.MenuItem>
+                    <Link to={`/games/${game.slug}`}>{game.name}</Link>
+                  </Dropdown.MenuItem>
+                ))}
+              </Dropdown.Menu>
+            </Dropdown>
+          ) : null}
+          <Link className="mr-5 hover:text-white" to="/privacy">
+            Privacy
+          </Link>
+          <Link className="hover:text-white" to="/settings">
+            Settings
+          </Link>
+        </nav>
+        {/* <button className="inline-flex items-center bg-gray-200 border-0 py-1 px-3 focus:outline-none hover:bg-gray-300 rounded text-base mt-4 md:mt-0">Button
       <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
         <path d="M5 12h14M12 5l7 7-7 7"></path>
       </svg>
     </button> */}
-  </div>
-</header>
-}
+      </div>
+    </header>
+  );
+};
