d5522b61017776175bb4e75f45d8d9c91d606603
Nick Radford <nick@nicholasradford.com>
2020-07-26 19:05:37 -0700

    set up endpoint for forwarding contact messages

---
+++ api/sendmail.ts
@@ -0,0 +1,33 @@
+import { NowRequest, NowResponse } from '@vercel/node';
+import sg, { MailService } from '@sendgrid/mail';
+
+sg.setApiKey(process.env.SG_API_KEY);
+
+const allowList = ['localhost:3000', 'gs.nickradford.dev'];
+
+export default (req: NowRequest, res: NowResponse) => {
+  const host = req.headers['x-forwarded-host'];
+
+  const { name, email, feedback } = req.body;
+
+  if (allowList.indexOf(host as string) !== -1) {
+    const msg: sg.MailDataRequired = {
+      to: process.env.CONTACT_FORM_TO,
+      from: 'no-reply@gs.nickradford.dev',
+      replyTo: email,
+      templateId: 'd-874e16697d0d467596249e3f80d0b285',
+      dynamicTemplateData: {
+        name,
+        email,
+        feedback,
+      },
+    };
+    sg.send(msg).then(() => {
+      res.status(200);
+      res.end();
+    });
+  } else {
+    res.statusCode = 406;
+    res.end();
+  }
+};
