1fe217d24da1c79b06e0227814c4ddc62ce3ffc2
Nick Radford <nick@nicholasradford.com>
2025-05-13 19:12:08 -0700

    vercel analytics

--- src/index.tsx
+++ src/index.tsx
@@ -7,8 +7,8 @@ import { PersistGate } from 'redux-persist/integration/react';
 import './tailwind.generated.css';
 import App from './App';
 import { store, persistor } from './redux/store';
+import { Analytics } from "@vercel/analytics/react"
 
-import * as serviceWorker from './serviceWorker';
 
 persistor.persist();
 
@@ -19,16 +19,12 @@ root.render(
     <Provider store={store}>
       <BrowserRouter>
         <>
-          {/* <PersistGate loading={null} persistor={persistor}> */}
-          <App />
-          {/* </PersistGate> */}
+          <PersistGate loading={null} persistor={persistor}>
+            <App />
+          </PersistGate>
         </>
       </BrowserRouter>
     </Provider>
+    <Analytics />
   </React.StrictMode>,
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
