0506f184a6ea831240301fabf2025e4efdba01f4
Nick Radford <nick@nicholasradford.com>
2025-05-13 23:25:31 -0700

    add logger

--- apps/api/api/index.ts
+++ apps/api/api/index.ts
@@ -1,11 +1,13 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
+import { logger } from 'hono/logger'
 
 export const config = {
   runtime: 'edge'
 }
 
 const api = new Hono().basePath('/api')
+api.use(logger())
 
 api.get('/', (c) => {
   return c.json({ message: 'Hello Hono!' })
